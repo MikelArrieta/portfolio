8f5384c3be3e5829a7939588670cdc58d1a681b1
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2022-09-07 09:28:17 +0200

    Scroll sections active link implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,24 @@ let swiper = new Swiper('.portfolio__container', {
       clickable: true,
    },
 });
+
+/*==================== Scroll sections active link ====================*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive () {
+   const scrollY = window.scrollY;
+
+   sections.forEach(current => {
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 50;
+      const sectionId = current.getAttribute('id');
+
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+         document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.add('active-link');
+      } else {
+         document.querySelector(`.nav__menu a[href*=${sectionId}]`).classList.remove('active-link');
+      }
+   });
+}
+
+window.addEventListener('scroll', scrollActive);
